5b321fa076fd3ffac10ef24311dbf9f32087c630
andreiAirinei <andreiAirinei@users.noreply.github.com>
2020-09-25 18:22:57 +0100

    Improved Device Buttons to scroll the main image accordingly. Revamped UI fonts, margins and padding to be more responsive

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 // ####### Block where Project's images (monitor, tablet, mobile) are taken care of ###### 
 
-// Select Project's image elements
+// Select Project's main image elements
 const recipeMainImage = document.querySelector('.recipe-main-image');
 const portfolioMainImage = document.querySelector('.portfolio-main-image');
 
+// Select device type buttons for each project
 const recipeMonitor = document.querySelector('.recipe-monitor');
 const recipeTablet = document.querySelector('.recipe-tablet');
 const recipeMobile = document.querySelector('.recipe-mobile');
@@ -13,13 +14,16 @@ const portfolioTablet = document.querySelector('.portfolio-tablet');
 const portfolioMobile = document.querySelector('.portfolio-mobile');
 
 // Event Functions
-const handleMainImage = e => {
+const handleDeviceClick = e => {
+  // Set active the current button pressed and disable the other ones
+  // Change main image according to device type
   switch (e.target.dataset.name) {
     case 'monitor':
       recipeMonitor.classList.add('isActive');
       recipeTablet.classList.remove('isActive');
       recipeMobile.classList.remove('isActive');
       recipeMainImage.src = './assets/tasty_recipes/monitor.jpg';
+      recipeQuickPeek();
       break;
 
     case 'tablet':
@@ -27,13 +31,15 @@ const handleMainImage = e => {
       recipeTablet.classList.add('isActive');
       recipeMobile.classList.remove('isActive');
       recipeMainImage.src = './assets/tasty_recipes/tablet.jpg';
+      recipeQuickPeek();
       break;
 
     case 'mobile':
       recipeMonitor.classList.remove('isActive');
       recipeTablet.classList.remove('isActive');
       recipeMobile.classList.add('isActive');
-      recipeMainImage.src = './assets/tasty_recipes/mobile.jpg'
+      recipeMainImage.src = './assets/tasty_recipes/mobile.jpg';
+      recipeQuickPeek();
       break;
 
     case 'portfolio-monitor':
@@ -41,6 +47,7 @@ const handleMainImage = e => {
       portfolioTablet.classList.remove('isActive');
       portfolioMobile.classList.remove('isActive');
       portfolioMainImage.src = './assets/tasty_recipes/portfolio_monitor.jpg';
+      portfolioQuickPeek();
       break;
 
     case 'portfolio-tablet':
@@ -48,6 +55,7 @@ const handleMainImage = e => {
       portfolioTablet.classList.add('isActive');
       portfolioMobile.classList.remove('isActive');
       portfolioMainImage.src = './assets/tasty_recipes/portfolio_tablet.jpg';
+      portfolioQuickPeek();
       break;
 
     case 'portfolio-mobile':
@@ -55,6 +63,7 @@ const handleMainImage = e => {
       portfolioTablet.classList.remove('isActive');
       portfolioMobile.classList.add('isActive');
       portfolioMainImage.src = './assets/tasty_recipes/portfolio_mobile.jpg';
+      portfolioQuickPeek();
       break;
 
     default:
@@ -62,12 +71,32 @@ const handleMainImage = e => {
   }
 }
 
-// Event Listeners
-recipeMonitor.addEventListener('click', handleMainImage);
-recipeTablet.addEventListener('click', handleMainImage);
-recipeMobile.addEventListener('click', handleMainImage);
-portfolioMonitor.addEventListener('click', handleMainImage);
-portfolioTablet.addEventListener('click', handleMainImage);
-portfolioMobile.addEventListener('click', handleMainImage);
+// Add Event Listeners for each button device type
+recipeMonitor.addEventListener('click', handleDeviceClick);
+recipeTablet.addEventListener('click', handleDeviceClick);
+recipeMobile.addEventListener('click', handleDeviceClick);
+portfolioMonitor.addEventListener('click', handleDeviceClick);
+portfolioTablet.addEventListener('click', handleDeviceClick);
+portfolioMobile.addEventListener('click', handleDeviceClick);
+
+
+// ### Functions
+
+// Manipulate main image scrolling
+const recipeQuickPeek = () => {
+  recipeMainImage.classList.add('translateImage-70');
+
+  setTimeout(() => {
+    recipeMainImage.classList.remove('translateImage-70');
+  }, 3500);
+}
+
+const portfolioQuickPeek = () => {
+  portfolioMainImage.classList.add('translateImage-70');
+
+  setTimeout(() => {
+    recipeMainImage.classList.remove('translateImage-70');
+  }, 3500);
+}
 
 
